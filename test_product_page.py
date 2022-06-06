1232d94d6857ec59636b1eae529f1a3276e97102
Andrew <gish.gish@inbox.ru>
2022-06-06 17:34:23 +0300

    uncomment first test and implicitly_wait

--- test_product_page.py
+++ test_product_page.py
@@ -7,20 +7,20 @@ import pytest
 from .pages.product_page import ProductPage
 
 
-# @pytest.mark.parametrize('promo_offer',
-#                          ["0", "1", "2", "3", "4", "5", "6",
-#                           pytest.param("7", marks=pytest.mark.xfail(reason='wrong book name after adding to basket')),
-#                           "8", "9"])
-# def test_guest_can_add_product_to_basket(browser, promo_offer):
-#     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
-#     page = ProductPage(browser, link)
-#     page.open()
-#
-#     page.add_to_basket()
-#     page.solve_quiz_and_get_code()
-#
-#     page.product_name_should_match_the_message()
-#     page.product_price_should_match_the_message()
+@pytest.mark.parametrize('promo_offer',
+                         ["0", "1", "2", "3", "4", "5", "6",
+                          pytest.param("7", marks=pytest.mark.xfail(reason='wrong book name after adding to basket')),
+                          "8", "9"])
+def test_guest_can_add_product_to_basket(browser, promo_offer):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
+    page = ProductPage(browser, link)
+    page.open()
+
+    page.add_to_basket()
+    page.solve_quiz_and_get_code()
+
+    page.product_name_should_match_the_message()
+    page.product_price_should_match_the_message()
 
 @pytest.mark.xfail(reason='Success message will always appear after adding product to basket')
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
